9511e9d19be8d7e29958f3e0da9c923125c6ec54
jonathan clark <thejonnyc9@gmail.com>
2026-04-27 11:33:46 -0400

    tronicmapmap

--- client_wasm/mainscene/helpers/rooms/create_tronic_positions.hpp
+++ client_wasm/mainscene/helpers/rooms/create_tronic_positions.hpp
@@ -1,15 +1,16 @@
 #pragma once
+#include <map>
 #include <memory>
-#include <vector>
 #include "tronic_position_map.hpp"
 
-std::vector<TronicPositionMap> create_tronic_positions(const Model& freddy) {
+std::map<std::string, TronicPositionMap> create_tronic_positions(
+    const Model& freddy) {
 
-  std::vector<TronicPositionMap> map_vector;
+  std::map<std::string, TronicPositionMap> tronicMapMap;
   TronicPositionMap freddy_map(freddy);
   freddy_map.set_position_map({{"toy_stage", {{0, 0, 0}, {0, 1, 0}, 0.0f}},
-                               {"stage", {{1, 0, 0}, {0, 1, 0}, 90.0f}}});
+                               {"stage", {{-1.0f, 4.5f, -42.0f}, {0, 1, 0}, 90.0f}}});
 
-  map_vector.emplace_back(freddy);
-  return map_vector
-};
+  tronicMapMap.emplace("freddy", freddy_map);
+  return tronicMapMap;
+}

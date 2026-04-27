db1282ebfe0c5aa2afb6c91a0a85f59a8133f838
jonathan clark <thejonnyc9@gmail.com>
2026-04-27 13:28:54 -0400

    draw

--- client_wasm/mainscene/helpers/rooms/create_tronic_positions.hpp
+++ client_wasm/mainscene/helpers/rooms/create_tronic_positions.hpp
@@ -20,12 +20,16 @@ inline std::map<std::string, TronicPositionMap> create_tronic_positions(
   std::map<std::string, TronicPositionMap> tronicMapMap;
   TronicPositionMap freddy_map(freddy);
   freddy_map.set_position_map(
-      {{"stage", {{-1.0f, 4.5f, -42.0f}, {0, 1, 0}, 90.0f}},
-       {"party_room", {{-2.0f, 4.5f, -30.0f}, {0, 1, 0}, 0.0f}},
-       {"lhs_hall", {{-4.0f, 4.5f, -18.0f}, {0, 1, 0}, 0.0f}},
+      {{"stage", {{-1.0f, 4.5f, -42.0f}, {0, 1, 0}, 0.0f}},
+       {"party_room", {{-2.0f, 3.0f, -30.0f}, {0, 1, 0}, 45.0f}},
+       {"lhs_hall", {{-6.0f, 3.0f, -17.0f}, {0, 1, 0}, 0.0f}},
        {"rhs_hall", {{4.0f, 4.5f, -18.0f}, {0, 1, 0}, 0.0f}},
-       {"lhs_closet", {{-4.5f, 4.5f, 0.0f}, {0, 1, 0}, 0.0f}},
-       {"rhs_closet", {{4.5f, 4.5f, 0.0f}, {0, 1, 0}, 0.0f}},
+       
+       {"lhs_closet", {{-3.5f, 3.0f, 2.5f}, {0, 1, 0}, 0.0f}},
+       {"rhs_closet", {{3.5f, 3.0f, 2.5f}, {0, 1, 0}, 0.0f}},
+
+
+
        {"p2_lhs_party", {{-8.0f, 52.0f, -12.0f}, {0, 1, 0}, 0.0f}},
        {"p2_rhs_hall", {{2.0f, 52.0f, -8.0f}, {0, 1, 0}, 0.0f}},
        {"p2_lhs_repair", {{-7.0f, 52.0f, -26.0f}, {0, 1, 0}, 0.0f}},
